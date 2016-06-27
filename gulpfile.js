469bc92396246220d1bb1a5e33fe532bea252e50
Greg <greg@flashtalking.com>
2016-06-27 10:19:07 +0100

    Implemented CSSNano, migrated from NPM scripts to Gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const	gulp				= require('gulp'),
+			cssnano			= require('gulp-cssnano'),
+			sass				= require('gulp-sass'),
+			rename			= require('gulp-rename'),
+			runSequence	= require('run-sequence'),
+			size				= require('gulp-size'),
+			source			= ['scss/**/*.scss']
+
+gulp.task('build', () => {
+	return gulp.src(source)
+		.pipe(sass())
+		.pipe(cssnano({
+			autoprefixer: { browsers: 'last 2 versions', add: true }
+		}))
+		.pipe(rename(function (path) {
+			path.extname = '.min.css'
+		}))
+		.pipe(gulp.dest('dist'))
+})
+
+gulp.task('demo', () => gulp.src('dist/blaze*.min.css').pipe(gulp.dest('demo')))
+
+gulp.task('file-size', () => {
+	return gulp.src('dist/blaze*.min.css')
+		.pipe(size({
+			gzip: true,
+			showFiles: true
+		}))
+})
+
+gulp.task('default', done => {
+	runSequence('build', 'demo', 'file-size', done)
+})
+
+// Rerun the task when a file changes
+gulp.task('watch', () => {
+	gulp.watch(source, ['default'])
+});
