3852a3af193eccd4a463c64fabef7f3a1df1918f
Greg <gregory.pratt@me.com>
2017-11-24 22:54:41 +0000

    restructure, making way for atoms

---
+++ scss/gulpfile.js
@@ -0,0 +1,48 @@
+const pkg = require('./package.json'),
+  gulp = require('gulp'),
+  options = require('gulp-options'),
+  cssnano = require('gulp-cssnano'),
+  sass = require('gulp-sass'),
+  sassLint = require('gulp-sass-lint'),
+  rename = require('gulp-rename'),
+  header = require('gulp-header'),
+  runSequence = require('run-sequence'),
+  size = require('gulp-size'),
+  fileExists = require('file-exists'),
+  source = ['./src/**/*.scss'];
+
+
+gulp.task('lint', () => gulp.src(source)
+    .pipe(sassLint({
+      configFile: './.scss-lint.yml'
+    }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+);
+
+gulp.task('build', () => {
+  let build = gulp.src(source)
+    .pipe(sass())
+    .pipe(cssnano({
+      autoprefixer: {browsers: 'last 2 versions', add: true},
+      zindex: false
+    }))
+    .pipe(rename(function (path) {
+      path.extname = '.min.css'
+    }))
+    .pipe(header('/*!v<%= pkg.version %>*/', {pkg}))
+    .pipe(gulp.dest('./dist'));
+
+  return build;
+});
+
+gulp.task('file-size', () => gulp.src('./dist/blaze*.min.css')
+    .pipe(size({
+      gzip: true,
+      showFiles: true
+    }))
+);
+
+gulp.task('demo', () => gulp.src('./dist/**/blaze*.min.css').pipe(gulp.dest('./demo')));
+gulp.task('default', done => runSequence('lint', 'build', 'demo', 'file-size', done));
+gulp.task('watch', () => gulp.watch(source, ['default']));
