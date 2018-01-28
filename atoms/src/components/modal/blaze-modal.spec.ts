90c21ba0bef3f7083fcf96de3aa5baa8bdb71726
Greg <gregory.pratt@me.com>
2018-01-28 15:33:48 +0000

    modal tests

--- atoms/src/components/modal/blaze-modal.spec.ts
+++ atoms/src/components/modal/blaze-modal.spec.ts
@@ -24,4 +24,70 @@ describe('Modal', () => {
     'renders correctly with no props',
     '<blaze-modal>default</blaze-modal>'
   );
+
+  snapIt(
+    'renders as open',
+    '<blaze-modal open>default</blaze-modal>'
+  );
+
+  snapIt(
+    'renders dismissible button and overlay',
+    '<blaze-modal open dismissible>default</blaze-modal>'
+  );
+
+  it('show modal method', async () => {
+    element = await render({
+      components: [Modal],
+      html: '<blaze-modal>default</blaze-modal>'
+    });
+    await flush(element);
+    expect(element).toMatchSnapshot();
+
+    element.show();
+    await flush(element);
+    expect(element.isOpen()).toEqual(true);
+    expect(element).toMatchSnapshot();
+  });
+
+  it('close modal method', async () => {
+    element = await render({
+      components: [Modal],
+      html: '<blaze-modal open>default</blaze-modal>'
+    });
+    await flush(element);
+    expect(element).toMatchSnapshot();
+
+    element.close();
+    await flush(element);
+    expect(element.isOpen()).toEqual(false);
+    expect(element).toMatchSnapshot();
+  });
+
+  it('close button', async () => {
+    element = await render({
+      components: [Modal],
+      html: '<blaze-modal open dismissible>default</blaze-modal>'
+    });
+    await flush(element);
+    expect(element).toMatchSnapshot();
+
+    element.querySelector('.c-button--close').click();
+    await flush(element);
+    expect(element.isOpen()).toEqual(false);
+    expect(element).toMatchSnapshot();
+  });
+
+  it('close by clicking overlay', async () => {
+    element = await render({
+      components: [Modal],
+      html: '<blaze-modal open dismissible>default</blaze-modal>'
+    });
+    await flush(element);
+    expect(element).toMatchSnapshot();
+
+    element.querySelector('.c-overlay--visible').click();
+    await flush(element);
+    expect(element.isOpen()).toEqual(false);
+    expect(element).toMatchSnapshot();
+  });
 });
