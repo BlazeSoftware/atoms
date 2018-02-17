b74625eeb98628984cd82d87e8f3a3d5ba93a661
Greg <gregory.pratt@me.com>
2018-02-17 17:45:02 +0000

    pagination refactor and tests

--- atoms/src/components/pagination/blaze-pagination.spec.ts
+++ atoms/src/components/pagination/blaze-pagination.spec.ts
@@ -20,6 +20,41 @@ describe('Pagination', () => {
     });
   };
 
-  snapIt('should work without parameters', '<blaze-pagination></blaze-pagination>');
-  snapIt('renders the correct buttons when pages set', '<blaze-pagination pages="15"></blaze-pagination>');
+  snapIt(
+    'should work without parameters',
+    '<blaze-pagination></blaze-pagination>'
+  );
+
+  snapIt(
+    'renders the correct buttons when pages set',
+    '<blaze-pagination pages="15"></blaze-pagination>'
+  );
+
+  it('has a go to page function', async () => {
+    element = await render({
+      components: [Pagination],
+      html: '<blaze-pagination pages="15"></blaze-pagination>'
+    });
+    await flush(element);
+    expect(element).toMatchSnapshot();
+
+    element.goToPage(4);
+    await flush(element);
+
+    expect(element).toMatchSnapshot();
+  });
+
+  it('emits a page changed event', async () => {
+    element = await render({
+      components: [Pagination],
+      html: '<blaze-pagination pages="15"></blaze-pagination>'
+    });
+    await flush(element);
+
+    element.addEventListener('onPageChange', (e) => {
+      expect(e.detail).toEqual(4);
+    });
+    element.goToPage(4);
+  });
+
 });
--- atoms/src/components/pagination/blaze-pagination.tsx
+++ atoms/src/components/pagination/blaze-pagination.tsx
@@ -1,4 +1,4 @@
-import { Component, Method, Prop, State } from '@stencil/core';
+import { Component, Event, EventEmitter, Method, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'blaze-pagination'
@@ -8,18 +8,13 @@ export class Pagination {
   @Prop() page: number = 1;
   @Prop() pages: number = 1;
   @State() _currentPage: number;
+  @Event() onPageChange: EventEmitter;
 
   @Method()
-  backwards() {
-    if (this._currentPage > 1) {
-      this._currentPage = this._currentPage - 1;
-    }
-  }
-
-  @Method()
-  forward() {
-    if (this._currentPage < this.pages) {
-      this._currentPage = this._currentPage + 1;
+  goToPage(page: number) {
+    if (page > 0 && page <= this.pages) {
+      this._currentPage = page;
+      this.onPageChange.emit(this._currentPage);
     }
   }
 
@@ -36,19 +31,19 @@ export class Pagination {
     return (
       <div class="c-pagination">
         <div class="c-pagination__controls c-pagination__controls--backward">
-          <button class="c-pagination__control" onClick={() => this._currentPage = 1} disabled={this._currentPage === 1}>&laquo;</button>
-          <button class="c-pagination__control" onClick={() => this.backwards()} disabled={this._currentPage === 1}>&lsaquo;</button>
+          <button class="c-pagination__control" onClick={() => this.goToPage(1)} disabled={this._currentPage === 1}>&laquo;</button>
+          <button class="c-pagination__control" onClick={() => this.goToPage(this._currentPage - 1)} disabled={this._currentPage === 1}>&lsaquo;</button>
         </div>
         <div class="c-pagination__controls">
           {this._currentPage > 1 && <span class="c-pagination__ellipsis">&hellip;</span>}
-          {this._currentPage > 1 && <button onClick={() => this.backwards()} class="c-pagination__page">{this._currentPage - 1}</button>}
+          {this._currentPage > 1 && <button onClick={() => this.goToPage(this._currentPage - 1)} class="c-pagination__page">{this._currentPage - 1}</button>}
           <button class="c-pagination__page c-pagination__page--current">{this._currentPage}</button>
-          {this._currentPage < this.pages && <button onClick={() => this.forward()} class="c-pagination__page">{this._currentPage + 1}</button>}
+          {this._currentPage < this.pages && <button onClick={() => this.goToPage(this._currentPage + 1)} class="c-pagination__page">{this._currentPage + 1}</button>}
           {this._currentPage < this.pages && <span class="c-pagination__ellipsis">&hellip;</span>}
         </div>
         <div class="c-pagination__controls c-pagination__controls--forward">
-          <button class="c-pagination__control" onClick={() => this.forward()} disabled={this._currentPage === this.pages}>&rsaquo;</button>
-          <button class="c-pagination__control" onClick={() => this._currentPage = this.pages} disabled={this._currentPage === this.pages}>&raquo;</button>
+          <button class="c-pagination__control" onClick={() => this.goToPage(this._currentPage + 1)} disabled={this._currentPage === this.pages}>&rsaquo;</button>
+          <button class="c-pagination__control" onClick={() => this.goToPage(this.pages)} disabled={this._currentPage === this.pages}>&raquo;</button>
         </div>
       </div>
     );
