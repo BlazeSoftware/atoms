965233afa4807c114467610767cd47c82aaefb64
Greg <gregory.pratt@me.com>
2018-02-26 21:34:16 +0000

    atom dist bugs

--- atoms/stencil.config.js
+++ atoms/stencil.config.js
@@ -1,23 +1,7 @@
 exports.config = {
   namespace: 'blaze-atoms',
   generateDistribution: true,
-  bundles: [
-    { components: ['blaze-accordion', 'blaze-accordion-pane'] },
-    { components: ['blaze-address', 'blaze-address-heading'] },
-    { components: ['blaze-alert'] },
-    { components: ['blaze-avatar'] },
-    { components: ['blaze-badge'] },
-    { components: ['blaze-breadcrumb', 'blaze-breadcrumbs'] },
-    { components: ['blaze-card', 'blaze-card-body', 'blaze-card-footer', 'blaze-card-header'] },
-    { components: ['blaze-drawer'] },
-    { components: ['blaze-image'] },
-    { components: ['blaze-modal'] },
-    { components: ['blaze-pagination'] },
-    { components: ['blaze-panel'] },
-    { components: ['blaze-tabs', 'blaze-tab'] },
-    { components: ['blaze-toasts'] },
-    { components: ['blaze-toggle'] }
-  ]
+  serviceWorker: false
 };
 
 exports.devServer = {
