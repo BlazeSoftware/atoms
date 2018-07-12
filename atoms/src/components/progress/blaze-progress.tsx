bb8bb99082dd4e38c57954a841c43409e34be32c
Daniel Weipert <code@drogueronin.de>
2018-07-12 18:33:10 +0200

    Progress with change and onChange Events

--- atoms/src/components/progress/blaze-progress.tsx
+++ atoms/src/components/progress/blaze-progress.tsx
@@ -1,12 +1,23 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Element, Event, EventEmitter, Listen, Prop } from '@stencil/core';
 
 @Component({
   tag: 'blaze-progress'
 })
 export class Progress {
 
+  @Element() private element: HTMLElement;
   @Prop() rounded: boolean;
   @Prop() size: string = '';
+  @Event() onChange: EventEmitter;
+
+  @Listen('change')
+  onChangeBar(ev) {
+    const progress = this.element.children[0];
+    const value = ev.detail;
+    const bar = ev.target;
+    const idx = [].indexOf.call(progress.children, bar);
+    this.onChange.emit({ idx, bar, ...value });
+  }
 
   render() {
     const sizeClass = this.size ? `u-${this.size}` : '';
