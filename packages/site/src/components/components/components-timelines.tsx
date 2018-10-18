56cfd6f7a9a09bd56b0fdc94c6be1a7ac993c3cf
Greg Pratt <Greg.Pratt@bjss.com>
2018-10-18 13:53:15 +0100

    docs(site): update timelines web page

--- packages/site/src/components/components/components-timelines.tsx
+++ packages/site/src/components/components/components-timelines.tsx
@@ -16,7 +16,7 @@ export class ComponentsTimelines {
 
         <blaze-tabs>
           <blaze-tab header="CSS" open>
-            <h2 id="single-selection" class="c-heading u-xlarge">
+            <h2 id="standard" class="c-heading u-xlarge">
               Standard
             </h2>
 
@@ -71,7 +71,7 @@ export class ComponentsTimelines {
 </ul>`}
             />
 
-            <h2 id="single-selection" class="c-heading u-xlarge">
+            <h2 id="colours" class="c-heading u-xlarge">
               Colours
             </h2>
 
@@ -126,7 +126,7 @@ export class ComponentsTimelines {
 </ul>`}
             />
 
-            <h2 id="single-selection" class="c-heading u-xlarge">
+            <h2 id="loading" class="c-heading u-xlarge">
               Loading last item
             </h2>
 
@@ -187,21 +187,93 @@ export class ComponentsTimelines {
   </li>
 </ul>`}
             />
+
+            <h2 id="alternate" class="c-heading u-xlarge">
+              Alternating items
+            </h2>
+
+            <blaze-demo
+              language="html"
+              code={`<ul class="o-timeline o-timeline--alternate o-timeline--loading">
+  <li class="c-timeline-item c-timeline-item--left">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">
+      Nothing
+    </div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--brand">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">
+      Big bang
+
+      <p class="c-paragraph u-text--quiet u-small">
+        Widely considered to be how the universe began
+      </p>
+    </div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--info c-timeline-item--left">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">Galaxies go everywhere</div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--warning">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">Solar systems form</div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--success c-timeline-item--left">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">Earth appears</div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--error">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">Human infestation occurs!</div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--left c-timeline-item--last">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">
+      All this has happened before, and will happen again
+    </div>
+  </li>
+  <li class="c-timeline-item c-timeline-item--loading">
+    <div class="c-timeline-item__tail"></div>
+    <div class="c-timeline-item__point"></div>
+    <div class="c-timeline-item__body">
+      ...Evolution
+    </div>
+  </li>
+</ul>`}
+            />
           </blaze-tab>
           <blaze-tab header="JavaScript">
             <blaze-demo
               demo={false}
               language="html"
-              code={`<blaze-alert open dismissible>Default</blaze-alert>
-<blaze-alert open dismissible type="brand">Brand</blaze-alert>
-<blaze-alert open dismissible type="info">Info</blaze-alert>
-<blaze-alert open dismissible type="warning">Warning</blaze-alert>
-<blaze-alert open dismissible type="success">Success</blaze-alert>
-<blaze-alert open type="error">Error (also not dismissible)</blaze-alert>`}
+              code={`<blaze-timeline alternate loading>
+  <blaze-timeline-item left>Nothing</blaze-timeline-item>
+  <blaze-timeline-item type="brand">
+    Big bang
+    <p class="c-paragraph">
+      Widely considered to be how the universe began
+    </p>
+  </blaze-timeline-item>
+  <blaze-timeline-item type="info">Galaxies go everywhere</blaze-timeline-item>
+  <blaze-timeline-item type="warning">Solar systems form</blaze-timeline-item>
+  <blaze-timeline-item type="success">Earth appears</blaze-timeline-item>
+  <blaze-timeline-item type="error">Human infestation occurs!</blaze-timeline-item>
+  <blaze-timeline-item last>All this has happened before and will happen again</blaze-timeline-item>
+  <blaze-timeline-item loading>Evolution</blaze-timeline-item>
+</blaze-timeline>`}
             />
 
             <h2 class="c-heading">Attributes</h2>
 
+            <h3 class="c-heading">Timeline</h3>
             <table class="c-table c-table--condensed">
               <thead class="c-table__head">
                 <tr class="c-table__row c-table__row--heading">
@@ -211,42 +283,32 @@ export class ComponentsTimelines {
               </thead>
               <tbody class="c-table__body">
                 <tr class="c-table__row">
-                  <td class="c-table__cell">open (boolean)</td>
-                  <td class="c-table__cell">Initial open/close state of the alert</td>
+                  <td class="c-table__cell">alternate (boolean)</td>
+                  <td class="c-table__cell">Styles the line of time centrally</td>
                 </tr>
                 <tr class="c-table__row">
-                  <td class="c-table__cell">dismissible (boolean)</td>
-                  <td class="c-table__cell">Allow the alert to be closed</td>
+                  <td class="c-table__cell">loading (boolean)</td>
+                  <td class="c-table__cell">Enables the loading item to occupy the last spot in the list</td>
                 </tr>
               </tbody>
             </table>
 
-            <h2 class="c-heading">Methods</h2>
-
-            <p class="c-paragraph">
-              To access public methods on the element first select it using{' '}
-              <code class="u-code">document.querySelector()</code>.
-            </p>
-
+            <h3 class="c-heading">Timeline item</h3>
             <table class="c-table c-table--condensed">
               <thead class="c-table__head">
                 <tr class="c-table__row c-table__row--heading">
-                  <th class="c-table__cell">Method</th>
+                  <th class="c-table__cell">Attribute</th>
                   <th class="c-table__cell">Description</th>
                 </tr>
               </thead>
               <tbody class="c-table__body">
                 <tr class="c-table__row">
-                  <td class="c-table__cell">show()</td>
-                  <td class="c-table__cell">Opens the alert</td>
+                  <td class="c-table__cell">last (boolean)</td>
+                  <td class="c-table__cell">Removes the line of time for this item</td>
                 </tr>
                 <tr class="c-table__row">
-                  <td class="c-table__cell">close()</td>
-                  <td class="c-table__cell">Hides the alert</td>
-                </tr>
-                <tr class="c-table__row">
-                  <td class="c-table__cell">isOpen()</td>
-                  <td class="c-table__cell">Promise&lt;true or false></td>
+                  <td class="c-table__cell">loading (boolean)</td>
+                  <td class="c-table__cell">Styles the item differently to indicate the content is not ready</td>
                 </tr>
               </tbody>
             </table>
