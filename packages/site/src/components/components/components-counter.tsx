450cf40fa7940a63e6b3cb3f28fe6e09fea727e7
Greg Pratt <Greg.Pratt@bjss.com>
2018-11-06 09:07:54 +0000

    docs(counter): add attributes, methods and events tables

--- packages/site/src/components/components/components-counter.tsx
+++ packages/site/src/components/components/components-counter.tsx
@@ -48,6 +48,85 @@ export class ComponentsCounter {
   <span slot="suffix"> in bank account</span>
 </blaze-counter>`}
         />
+
+        <h2 class="c-heading">Attributes</h2>
+
+        <table class="c-table">
+          <thead class="c-table__head">
+            <tr class="c-table__row c-table__row--heading">
+              <th class="c-table__cell">Name</th>
+              <th class="c-table__cell">Description</th>
+            </tr>
+          </thead>
+          <tbody class="c-table__body">
+            <tr class="c-table__row">
+              <td class="c-table__cell">start: number</td>
+              <td class="c-table__cell">The initial value of the counter</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">value: number</td>
+              <td class="c-table__cell">The value the counter will end on</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">duration: number (seconds)</td>
+              <td class="c-table__cell">The time in seconds the counter will take to go from start to value</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">delay: number (millisecond)</td>
+              <td class="c-table__cell">How long the counter will wait before starting</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">decimals: number</td>
+              <td class="c-table__cell">Number of decimal places to display</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h2 class="c-heading">Methods</h2>
+
+        <table class="c-table">
+          <thead class="c-table__head">
+            <tr class="c-table__row c-table__row--heading">
+              <th class="c-table__cell">Name</th>
+              <th class="c-table__cell">Description</th>
+            </tr>
+          </thead>
+          <tbody class="c-table__body">
+            <tr class="c-table__row">
+              <td class="c-table__cell">reset(): void</td>
+              <td class="c-table__cell">Resets the display to the initial start value</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">restart(): void</td>
+              <td class="c-table__cell">Will reset the display and it will immediately start the counter again</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">pauseResume(): void</td>
+              <td class="c-table__cell">Pause or resume the counter</td>
+            </tr>
+            <tr class="c-table__row">
+              <td class="c-table__cell">update(value: number): void</td>
+              <td class="c-table__cell">Changes the value and automatically starts the counter</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h2 class="c-heading">Events</h2>
+
+        <table class="c-table">
+          <thead class="c-table__head">
+            <tr class="c-table__row c-table__row--heading">
+              <th class="c-table__cell">Name</th>
+              <th class="c-table__cell">Description</th>
+            </tr>
+          </thead>
+          <tbody class="c-table__body">
+            <tr class="c-table__row">
+              <td class="c-table__cell">complete: void</td>
+              <td class="c-table__cell">Trigged when the counter has finished</td>
+            </tr>
+          </tbody>
+        </table>
       </page-template>
     );
   }
