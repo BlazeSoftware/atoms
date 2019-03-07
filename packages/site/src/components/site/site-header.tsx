9cee5245e82044c05b51e9d8d3a3597b0d4c25a9
Greg Pratt <Greg.Pratt@bjss.com>
2019-03-07 16:31:47 +0000

    style(homepage): togglz ad

--- packages/site/src/components/site/site-header.tsx
+++ packages/site/src/components/site/site-header.tsx
@@ -1,9 +1,17 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
   tag: 'site-header',
 })
 export class SiteHeader {
+  @State()
+  features: any;
+
+  async componentWillLoad() {
+    const request = await fetch('https://www.togglz.com/features/8bjlqwqpxlg8nmvfz6av', { mode: 'cors' });
+    this.features = await request.json();
+  }
+
   render() {
     return (
       <header class="o-page-header o-container o-container--large">
@@ -11,6 +19,15 @@ export class SiteHeader {
           Skip to main content
         </a>
         <site-nav />
+        {this.features.togglz_ad && (
+          <div class="o-togglz-ad">
+            <blaze-alert dismissible open>
+              <a href="https://www.togglz.com" class="c-link">
+                Check out Togglz - a feature toggle service for websites and apps
+              </a>
+            </blaze-alert>
+          </div>
+        )}
         <div class="u-centered u-letter-box-small">
           <a aria-label="Blaze Home Page" href="/" onFocus={() => document.querySelector('site-nav').close()}>
             <i aria-hidden class="o-page-logo fas fa-fire fa-4x" />
