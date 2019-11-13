f882d12272fdf8bb9baae6d0edc87d2511026c8d
Greg <greg@gregs-mbp.home>
2019-11-13 12:58:44 +0000

    style(site): nice home page icons
    also updated font awesome to the new method

--- packages/site/src/components/home/home-page.tsx
+++ packages/site/src/components/home/home-page.tsx
@@ -29,95 +29,90 @@ export class LandingPage {
           </stencil-route-link>
         </div>
 
-        <div class="o-homepage-sections u-centered">
-          <div class="c-homepage-section">
-            <div class="o-container o-container--super">
-              <div class="o-grid o-grid--wrap o-grid--xsmall-full o-grid--small-full o-grid--medium-full">
-                <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
-                  <i class="fab fa-accessible-icon fa-5x" style={{ color: '#74748c' }} />
+        <div class="o-container o-container--super u-centered">
+          <div class="o-grid o-grid--wrap o-grid--xsmall-full o-grid--small-full o-grid--medium-full">
+            <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
+              <i class="fa-fw fab fa-accessible-icon fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Accessibility</h3>
+              <h3 class="c-heading u-xlarge">Accessibility</h3>
 
-                  <p class="c-paragraph">
-                    <span class="u-text--loud">Accessibile selectors</span> help to educate the importance of enabling
-                    everyone to access your site.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
-                  <i class="fab fa-js fa-5x" style={{ color: '#4267ff' }} />
+              <p class="c-paragraph">
+                <span class="u-text--loud">Accessibile selectors</span> help to educate the importance of enabling
+                everyone to access your site.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
+              <i class="fa-fw fab fa-js-square fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Framework free</h3>
+              <h3 class="c-heading u-xlarge">Framework free</h3>
 
-                  <p class="c-paragraph">
-                    <span class="u-text--loud">Any framework</span>, or no framework at all. You're not locked in. It's
-                    your choice.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
-                  <i class="fas fa-hand-paper fa-5x" style={{ color: '#088a05' }} />
+              <p class="c-paragraph">
+                <span class="u-text--loud">Any framework</span>, or no framework at all. You're not locked in. It's your
+                choice.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
+              <i class="fa-fw fas fa-hand-paper fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Opt-in</h3>
+              <h3 class="c-heading u-xlarge">Opt-in</h3>
 
-                  <p class="c-paragraph">
-                    Blaze won't automatically take control of any of your design unless you want it to.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
-                  <i class="fas fa-mobile fa-5x" style={{ color: '#4E5851' }} />
+              <p class="c-paragraph">
+                Blaze won't automatically take control of any of your design unless you want it to.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-window-box-super">
+              <i class="fa-fw fas fa-mobile fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Responsive</h3>
+              <h3 class="c-heading u-xlarge">Responsive</h3>
 
-                  <p class="c-paragraph">All components are developed mobile first and work on any screen size.</p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
-                  <i class="fas fa-cogs fa-5x" style={{ color: '#ffa500' }} />
+              <p class="c-paragraph">All components are developed mobile first and work on any screen size.</p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
+              <i class="fa-fw fas fa-cogs fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Custom builds</h3>
+              <h3 class="c-heading u-xlarge">Custom builds</h3>
 
-                  <p class="c-paragraph">
-                    In 3 easy steps you can have a custom build with easy to use variables and mixins.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
-                  <i class="fas fa-code fa-5x" style={{ color: '#74748c' }} />
+              <p class="c-paragraph">
+                In 3 easy steps you can have a custom build with easy to use variables and mixins.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
+              <i class="fa-fw fas fa-code fa-5x" style={{ color: '#74748c' }} />
 
-                  <h3 class="c-heading u-xlarge">#UseThePlatform</h3>
+              <h3 class="c-heading u-xlarge">#UseThePlatform</h3>
 
-                  <p class="o-paragraph">
-                    Our components rely solely on native browser features, not a separate library or framework.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-100">
-                  <h2 class="c-heading u-super">
-                    <blaze-divider>Open Source</blaze-divider>
-                  </h2>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
-                  <i class="fab fa-github fa-5x" style={{ color: '#8bc8f9' }} />
+              <p class="o-paragraph">
+                Our components rely solely on native browser features, not a separate library or framework.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-100">
+              <h2 class="c-heading u-super">
+                <blaze-divider>Open Source</blaze-divider>
+              </h2>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
+              <i class="fa-fw fab fa-github fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Available to everyone</h3>
+              <h3 class="c-heading u-xlarge">Available to everyone</h3>
 
-                  <p class="c-paragraph">
-                    This project is totally free. It's hosted on Github and is access to everyone.
-                  </p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
-                  <i class="fas fa-gavel fa-5x" style={{ color: '#d3eafd' }} />
+              <p class="c-paragraph">
+                This project is totally free. It's hosted on Github and is accessible by everyone.
+              </p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
+              <i class="fa-fw fas fa-gavel fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">MIT Licence</h3>
-                  <p class="c-paragraph">You can benefit from the project however you want. In your own way.</p>
-                </div>
-                <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
-                  <i class="fas fa-code-branch fa-5x" style={{ color: '#0c80df' }} />
+              <h3 class="c-heading u-xlarge">MIT Licence</h3>
+              <p class="c-paragraph">You can benefit from the project however you want. In your own way.</p>
+            </div>
+            <div class="o-grid__cell o-grid__cell--width-33 u-letter-box-super">
+              <i class="fa-fw fas fa-code-branch fa-5x" />
 
-                  <h3 class="c-heading u-xlarge">Contributing</h3>
+              <h3 class="c-heading u-xlarge">Contributing</h3>
 
-                  <p class="c-paragraph">
-                    See something wrong or want some new feature? Raise an issue or pull request. All feedback is
-                    welcome.
-                  </p>
-                </div>
-              </div>
+              <p class="c-paragraph">
+                See something wrong or want some new feature? Raise an issue or pull request. All feedback is welcome.
+              </p>
             </div>
           </div>
         </div>
